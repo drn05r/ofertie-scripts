e7dfa0d7ee6d04f30bc0249d186a33de09cff4dc
David R Newman <drn@ecs.soton.ac.uk>
2014-04-07 16:58:45 +0100

    Fixed issues with installer.

--- tve_ipv6_and_multicast/install.bash
+++ tve_ipv6_and_multicast/install.bash
@@ -1,56 +1,49 @@
 #!/bin/bash
 d=`dirname $0`
 BASE_DIR=`cd ${d}; pwd`
-
-echo "Installing package based dependencies"
-#sudo apt-get update || { echo "Could not update package list using APT ... aborting!"; exit 1; }
-#sudo apt-get install -y autoconf automake bison bridge-utils build-essential bzip2 cmake debhelper dkms fakeroot flex git graphviz libboost-devlibc6-dev libpcap-dev libpcre3-dev libssl-dev libtool libxerces-c2-dev make module-init-tools netbase openssl pkg-config procps python python-all python-argparse python-nose python-pexpect python-qt4 python-twisted-conch python-zopeinterface traceroute unzip uuid-runtime vlan wget || { echo "Could not install all package dependencies ... aborting!"; exit 1; }
+BUILD_DIR="${BASE_DIR}/dependencies"
 
 cd ${BUILD_DIR}
-echo "Downloading non-package dependencies"
+echo -e "\n\nDownloading dependencies\n"
 if [ ! -d mininet ]; then
-	git clone git://github.com/mininet/mininet || echo "Could not clone Git respository for Mininet ... aborting!"; exit 1; }
-fi
-#if [ ! -d ofsoftswitch13 ]; then
-#	git clone https://github.com/CPqD/ofsoftswitch13.git || echo "Could not clone Git respository for OpenFlow 1.3 Soft Switch ... aborting!"; exit 1; }
-#fi
-#if [ ! -d nox13oflib ]; then
-#	git clone https://github.com/CPqD/nox13oflib.git || echo "Could not clone Git respository for OpenFlow 1.3 modifieed NOX controller ... aborting!"; exit 1; }
+	echo "Git cloning Mininet"
+	git clone git://github.com/mininet/mininet || { echo -e "\n\nCould not clone Git respository for Mininet ... aborting!\n"; exit 1; }
 fi
 if [ ! -d iperf ]; then
-	git clone https://github.com/esnet/iperf.git  echo "Could not clone Git respoistory for iperf3 ... aborting!"; exit 1; }
+	echo "Git cloning Iperf"
+	git clone https://github.com/esnet/iperf.git || { echo -e "\n\nCould not clone Git respository for iperf3 ... aborting!\n"; exit 1; }
 fi
-#if [ ! -f nbeesrc-12-05-16.zip ]; then
-#	wget http://www.nbee.org/download/nbeesrc-12-05-16.php || { echo "Could not download nbeesrc-12-05-16.zip ... exiting!"; exit 1; }
-#	unzip nbeesrc-12-05-16.zip || { echo "Could not unzip nbeesrc-12-05-16.zip ... aborting!"; exit 1; }
-#fi
 
-echo "\nInstalling Mininet with OpenFlow Software Switch and Modified NOX Controller"
-#sudo modprobe 8021q || { echo "Could not enable 8021q kernel module for vlans ... aborting!"; exit 1; }
-mininet/util/install.sh -3fxn || { echo "Could not install Mininet core files and dependencies ... aborting!"; exit 1; }
 
-#echo -e "\nInstalling NetBee (Dependency of OpenFlow 1.3 Software Switch)"
-#
-#cd nbeesrc/src || { echo "No nbeesrc/src/ directory in ${BUILD_DIR} ... aborting!"; exit 1;}
-#cmake . || { echo "Could not cmake NetBee from source ... aborting!"; exit 1; }
-#make || { echo "Could not make NetBee from source ... aborting!"; exit 1; }
-#sudo cp ../bin/libn*.so /usr/local/lib || { echo "Could not copy NetBee shared object files to /usr/local/lib/ ... aborting!"; exit 1; }
-#sudo ldconfig || { echo "ldconfig failed to run successfully ... aborting!"; exit 1; }
-#sudo cp -R ../include/* /usr/include/ || { echo "Could not copy NetBee include files to /usr/include/ ... aborting!"; exit 1; }
+echo -e "\n\nInstalling useful networking features and utilities\n"
+sudo apt-get update || { echo -e "\n\nCould not update package list for APT ... aborting!\n"; exit 1; }
+sudo apt-get install -y vlan bridge-utils nmap || { echo -e "\n\nCould not install selected network feature and utility packages ... aborting!\n"; exit 1; }
+sudo modprobe 8021q || { echo -e "\n\nCould not enable 8021q kernel module for vlans ... aborting!\n"; exit 1; }
 
-#echo -e "\nInstalling OpenFlow 1.3 Software Switch"
-#cd ${BUILD_DIR}/ofsoftswitch13
-#./boot.sh || { echo "Could not run boot.sh for OpenFlow 1.3 Software Switch ... aborting!";  exit 1; }
-#./configure || { echo "Could not run configure for OpenFlow 1.3 Software Switch ... aborting!";  exit 1; }
-#make || { echo "Could not make OpenFlow 1.3 Software Switch ... aborting!";  exit 1; }
-#sudo make install || { echo "Could not make install OpenFlow 1.3 Software Switch ... aborting!";  exit 1; }
-#cd ${BUILD_DIR}
-#
-#echo -e "\nInstall OpenFlow 1.3 Modified NOX Controller"
-#cd ${BUILD_DIR}/nox13oflib  
-#./boot.sh || { echo "Could not run boot.sh for OpenFlow 1.3 Modified NOX Controller ... aborting!";  exit 1; }
-#mkdir build || { echo "Could not create build directory for OpenFlow 1.3 Modified NOX Controller ... aborting!";  exit 1; }
-#cd build 
-#../configure --with-python=no || { echo "Could not run configure for OpenFlow 1.3 Modified NOX Controller ... aborting!";  exit 1; }
-#make { echo "Could not make for OpenFlow 1.3 Modified NOX Controller ... aborting!";  exit 1; }
-#
+echo -e "\n\nInstalling Mininet with OpenFlow Software Switch and Modified NOX Controller\n"
+sudo mininet/util/install.sh -3fxn || { echo -e "\n\nCould not install Mininet core files and dependencies ... aborting!\n"; exit 1; }
+
+architecture=`arch`
+if [ -f ${BUILD_DIR}/${architecture}/iperf3/iperf3 ]; then
+	echo -e "\n\nInstalling Iperf3 binary (compiled for ${architecture} architectures)\n"
+	sudo cp $BUILD_DIR/$architecture/iperf3/iperf3 /usr/local/bin/
+else
+	echo -e "\n\nERROR: Iperf3 needs to be built from source for your architecture (${architecture}).  Follow the instructions in ${BUILD_DIR}/iperf/INSTALL\n"
+	exit 1;
+fi
+
+echo -e "\n\nInfrastructure for running experiments tests has been successfully installed.  You can run the following experiment test sets:
+	basicIPv6
+	basicIPv6Multicast
+	complexIPv6
+
+These can either be run once directly using python:
+
+	sudo python ${BASE_DIR}/<testset_name>Tests.py
+
+Or multiple times using the runTestsRepeated.bash script:
+
+	sudo ${BASE_DIR}/runTestsRepeated.bash <testset_name>Tests.py <iterations>
+	
+	E.g. ${BASE_DIR}/runTestsRepeated.bash basicIPv6Tests.py 10\n"
+
